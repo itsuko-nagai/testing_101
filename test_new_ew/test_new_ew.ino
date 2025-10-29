4bd1adb3048cdb46a6e87f1e32bd228eee0a0467
itsuko-nagai <itsukonagai20@gmail.com>
2025-10-29 23:43:27 +0530

    changed code

--- test_new_ew/test_new_ew.ino
+++ test_new_ew/test_new_ew.ino
@@ -1,9 +1,15 @@
+int hi = 0;
+
 void setup() {
-  pinMode(12, OUTPUT)
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+  Serial.print(hi);
 }
